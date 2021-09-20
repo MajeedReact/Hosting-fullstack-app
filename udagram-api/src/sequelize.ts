a7d9b15ed0ee0a7ef5f2aaeebb37d59ee60df71f
MajeedReact <mjalsalhi@gmail.com>
2021-09-20 22:37:42 +0300

    env updated

--- udagram-api/src/sequelize.ts
+++ udagram-api/src/sequelize.ts
@@ -2,10 +2,10 @@ import { Sequelize } from "sequelize-typescript";
 import { config } from "./config/config";
 
 export const sequelize = new Sequelize({
-  username: "postgres",
-  password: "postgres",
-  database: "postgres",
-  host: "database-1.c9kmlpsxzlp2.us-east-1.rds.amazonaws.com",
+  username: config.username,
+  password: config.password,
+  database: config.database,
+  host: config.host,
   port: 5432,
 
   dialect: "postgres",
